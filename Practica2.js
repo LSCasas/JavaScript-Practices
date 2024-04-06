f494a32e2e02f88aa69d027c55e88652d85c0ffc
Luis Casas <luisscasas45@gmail.com>
2024-04-05 20:21:02 -0600

    feat: add change to my calculator IMC

--- Practica2.js
+++ Practica2.js
@@ -1,17 +1,32 @@
-function calcularIMC(peso, estatura) {
- const resultado = peso / (estatura*estatura);
-console.log(resultado)
- if (resultado <18.5) {
-    console.log("Bajo peso");
- } else if ( 18.5<resultado >24.9) {
-    console.log("Peso Saludable");
- } else if (24.9 < resultado > 29.9) {
-    console.log("Sobrepeso");
- } else { 
-    console.log("obesidad");
- }
 
- return resultado;
+//Calculadora IMC
 
+
+//funcion del porgrama, va a realizar la operacion para poder lograr obtener el IMC
+function calcularIMC (pesoParametro,alturaParametro){
+    const IMC = pesoParametro / (alturaParametro ** 2)
+console.log("tu indice de masa corporal es", IMC);
+return IMC;
 }
-console.log (calcularIMC (62,164))
+
+//cons categoria: da una categoria debendiendo de cual sea tu IMC, desde peso saludame 0 > a <18.5 que es el peso saludable y IMC >29.9 contara como obecidad
+const darCategoriaIMC = (IMC) => {
+    if (isNaN(IMC)) {
+        console.log("Error: Los valores iniciales no son validos.")
+    }
+    else if (IMC < 18.5 ) {
+        console.log("Tu categoria es bajo peso")
+    } else if (IMC <24.9) {
+        console.log("Tu categoria es saludable")
+    } else if (IMC <29.9) {
+        console.log("Tu categoria es Sobrepeso")
+    } else {
+        console.log("Tu categoria es Obesidad")
+    }
+}
+
+//Ejemplo de uso
+const peso = 71;
+const altura = 1.89;
+
+darCategoriaIMC(calcularIMC(peso, altura));
