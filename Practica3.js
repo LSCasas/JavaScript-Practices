09cb389f31104d65d8d474b4dece258eafb1c17c
Luis Casas <luisscasas45@gmail.com>
2024-04-09 18:21:53 -0600

    feat: add practice 3

---
+++ Practica3.js
@@ -0,0 +1,45 @@
+const libro1 = {
+    Titulo: "Cien años de soledad",
+    Autor : "Gabriel García Márquez",
+    Género : "Ficción",
+    Publicación: "1967",
+    Editorial: "Editorial Sudamericana"
+};
+
+console.log(libro1.Titulo)
+console.log(libro1.Autor)
+console.log(libro1.Género)
+console.log(libro1.Publicación)
+console.log(libro1.Editorial)
+
+const libro2 = {
+    Titulo: "El Señor de los Anillos",
+    Autor : "J.R.R. Tolkien",
+    Género : "Fantasía",
+    Publicación: "1954",
+    Editorial: "George Allen & Unwin (Reino Unido), Houghton Mifflin (Estados Unidos)"
+};
+
+
+console.log(libro1.Titulo)
+console.log(libro1.Autor)
+console.log(libro1.Género)
+console.log(libro1.Publicación)
+console.log(libro1.Editorial)
+
+
+const libro3 = {
+    Titulo: "Breve historia del tiempo",
+    Autor : "Stephen Hawking",
+    Género : "Divulgación científica",
+    Publicación: "1988",
+    Editorial: "Bantam Books"
+    
+};
+
+
+console.log(libro1.Titulo)
+console.log(libro1.Autor)
+console.log(libro1.Género)
+console.log(libro1.Publicación)
+console.log(libro1.Editorial)
