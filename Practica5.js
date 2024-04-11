8fbbe065bb9ffa0426f5fac77b4862dc550671cb
Luis Casas <luisscasas45@gmail.com>
2024-04-10 18:17:07 -0600

    feat: new change in practice 5

--- Practica5.js
+++ Practica5.js
@@ -1,18 +1,26 @@
-const imprimirElemento = (texto, index, eliminar) => {
-    const palabras = texto.split(" "); // Separar la frase en palabras y almacenarlas en un array
+// Abstraccion
+//1. La frase que se quiere seleccionar debe estar dentro de un array
+//2. Para seleccionar su posicion utilizara los argumentos texto, index
+//3. Se necesita usar condicionales if, else
+//4. Se utiliza el condicional if (condicion: la palabra que seleccionaste es mayor al numero de palabras que hay del array o que sea menor 0)
+//5. en este caso debe imprimir "solo se pueden seleccionar palabras de la 0 a la 8"
 
-    if (index > palabras.length) {
-        console.log("No se puede imprimir arrays mayores al número de palabras");
+
+const imprimirElemento = (texto, index) => {
+    const palabras = texto.split(" "); 
+
+    if (index >= palabras.length || index < 0) {
+        console.log("Solo se pueden imprimir palabras de la 0 a 8");
     } else {
-        const palabraSeleccionada = palabras[index - 1]; // Acceder a la palabra del array según el índice especificado
+        const palabraSeleccionada = palabras[index]; 
         console.log("La palabra seleccionada es:", palabraSeleccionada);
         console.log("Número de letras de la palabra seleccionada:", palabraSeleccionada.length);
         
-        palabras.splice(index - 1, 1); // Eliminar la palabra seleccionada del array
+        palabras.splice(index, 1);
 
         console.log("Frase sin la palabra seleccionada:", palabras.join(" "));
     }
 };
 
 const frase = "La mejor forma de predecir el futuro es creandolo";
-imprimirElemento(frase, 5); // Ejemplo de uso: Seleccionar la quinta palabra
+imprimirElemento(frase, 0); 
